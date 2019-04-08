ba4dc2c72eb7c94756df51e11d73e44fcf4980fd
Spiritc <32603371+Spiritc@users.noreply.github.com>
2019-04-08 14:41:52 +0300

    https://github.com/Spiritc/RandomCode

---
+++ MordorsCruelPlan/Factories/MoodFactory.cs
@@ -0,0 +1,29 @@
+﻿using MordorsCruelPlan.Moods;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MordorsCruelPlan.Factories
+{
+    public class MoodFactory
+    {
+      public Mood CreateMood(string type)
+        {
+            type = type.ToLower();
+
+            switch (type)
+            {
+                case "angry":
+                    return new Angry();
+                case "sad":
+                    return new Sad();
+                case "happy":
+                    return new Happy();
+                case "javascript":
+                    return new JavaScript();
+                default:
+                    throw new Exception("Invalid Mood Type!");
+            }
+        }
+    }
+}
---
+++ MordorsCruelPlan/Foods/Apple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MordorsCruelPlan.Foods
+{
+    public class Apple : Food
+    {
+        public const int happiness = 1;
+
+        public Apple() 
+            : base(happiness)
+        {
+        }
+    }
+}
