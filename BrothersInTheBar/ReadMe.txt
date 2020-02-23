6c7760ade721e8ca3252d4ce8331c739d9550db2
Spiritc <32603371+Spiritc@users.noreply.github.com>
2020-02-23 20:26:15 +0200

    Add files via upload

---
+++ BrothersInTheBar/ReadMe.txt
@@ -0,0 +1,22 @@
+﻿Step 1:
+     We declare variables and retrieve information from the console
+
+Step 2:
+     There are two ways to enter the size of glasses:
+     a. on one line with dividers ' ' or/and ',' , we can add more 
+        characters or symbols as dividers depending on the format of the input
+     b. or one glasse size per line
+
+Step 3:
+     We call a function "Rounds" with a parameter array of cup sizes and returns 
+     how many times can we find 3 identical glasses next to each other
+
+Step 4:
+     Creating variables and copying the array to work with
+
+Step 5:
+     "While" and "For" cycles that make the necessary checks on the array 
+     and adds the found sequences to "count" variable
+
+Step 6:
+     Program ends with printing the number of glasses that brothers drink
---
+++ BrothersInTheBar/StartUp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace BrothersInTheBar
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Step 1
+            int finalRounds = 0;
+            int inputGlassesCount = int.Parse(Console.ReadLine());
+            int[] barGlasses = new int[inputGlassesCount];
+            string inputGassesSize = Console.ReadLine();
+            int numberOfGlasses = inputGassesSize.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray().Length;
+
+            //Step 2
+            if (numberOfGlasses > 1)
+            {
+                string[] inputGlasses = inputGassesSize.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                for (int i = 0; i < inputGlassesCount; i++)
+                {
+                    barGlasses[i] = int.Parse(inputGlasses[i]);
+                }
+            }
+            else
+            {
+                barGlasses[0] = int.Parse(inputGassesSize);
+                for (int i = 1; i < inputGlassesCount; i++)
+                {
+                    barGlasses[i] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            //Step 3
+            finalRounds = Rounds(barGlasses);
+            //Step 6
+            Console.WriteLine(finalRounds);
+
+        }
+
+
+        private static int Rounds(int[] barGlasses)
+        {
+            //Step 4
+            int count = 0;
+            bool foundRound = false;
+            int[] barGlassesRounds = new int[barGlasses.Length];
+            for (int i = 0; i < barGlasses.Length; i++)
+            {
+                barGlassesRounds[i] = barGlasses[i];
+            }
+
+            //Step 5
+            while (true)
+            {
+
+                for (int i = 0; i < barGlassesRounds.Length - 2; i++)
+                {
+                    if (barGlassesRounds[i] == barGlassesRounds[i + 1] && barGlassesRounds[i] == barGlassesRounds[i + 2])
+                    {
+                        count++;
+                        barGlassesRounds[i] = 0;
+                        barGlassesRounds[i + 1] = 0;
+                        barGlassesRounds[i + 2] = 0;
+                        foundRound = true;
+                    }
+                }
+
+                if (foundRound)
+                {
+                    barGlassesRounds = barGlassesRounds.Where(x => x != 0).ToArray();
+                    foundRound = false;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return count;
+        }
+    }
+}
