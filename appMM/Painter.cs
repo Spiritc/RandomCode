d0ee443b77afa8b1efb092c5cd484a22eb848d0d
Spiritc <32603371+Spiritc@users.noreply.github.com>
2020-02-24 15:54:31 +0200

    Add files via upload

--- appMM/Painter.cs
+++ appMM/Painter.cs
@@ -6,6 +6,7 @@ namespace appM
 {
     class Painter
     {
+        //Who else?
         private string name = "Leonardo";
         private int magicNumber;
 
@@ -22,6 +23,8 @@ namespace appM
         {
             int brushRows = 0;
             int brushColumns = this.MagicNumber;
+
+            //Our rows have to be even number
             if (this.MagicNumber % 2 == 0)
             {
                 brushRows = this.MagicNumber;
@@ -32,6 +35,7 @@ namespace appM
             }
 
 
+            //This "for" is printing Uper half of the painting
             for (int i = 0; i < brushRows/2; i++)
             {
                 Console.WriteLine((new string('-', brushColumns - i)) + (new string('*', brushColumns + i * 2)) +
@@ -40,6 +44,8 @@ namespace appM
                                   (new string('*', brushColumns + i * 2)) + (new string('-', brushColumns - i * 2)) +
                                   (new string('*', brushColumns + i * 2)) + (new string('-', brushColumns - i)));
             }
+
+            //This "for" is printing lower half of the painting
             for (int i = 0; i < brushRows / 2; i++)
             {
                 Console.WriteLine((new string('-', brushColumns/2 - i)) + (new string('*', brushColumns)) + (new string('-', 1 + i * 2)) + 
